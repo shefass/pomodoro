458cfc77fe3a62c53e9957d046657a99afedc673
Shefass <shefass@gmail.com>
2019-03-03 23:28:33 +0200

    Updates

---
+++ precache-manifest.c0e6c318acf913a80508c362aaf2386f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/pomodoro/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "a191527555aa94430439",
+    "url": "/pomodoro/static/css/main.4a000d69.chunk.css"
+  },
+  {
+    "revision": "0dad2b6a448e36f6e3ba",
+    "url": "/pomodoro/static/js/runtime~main.0dad2b6a.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/pomodoro/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "4915e2889e36a9cb61c2",
+    "url": "/pomodoro/static/js/2.4915e288.chunk.js"
+  },
+  {
+    "revision": "dabc3535fcfde72bfbe44f9efe699d5b",
+    "url": "/pomodoro/static/media/pianoNot.dabc3535.mp3"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/pomodoro/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/pomodoro/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/pomodoro/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/pomodoro/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/pomodoro/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "a191527555aa94430439",
+    "url": "/pomodoro/static/js/main.a1915275.chunk.js"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/pomodoro/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/pomodoro/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/pomodoro/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/pomodoro/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/pomodoro/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/pomodoro/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/pomodoro/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/pomodoro/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/pomodoro/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "4915e2889e36a9cb61c2",
+    "url": "/pomodoro/static/css/2.16b6a83a.chunk.css"
+  },
+  {
+    "revision": "c43b499bb3fd27fa08d1c4410282b5a2",
+    "url": "/pomodoro/index.html"
+  }
+];
